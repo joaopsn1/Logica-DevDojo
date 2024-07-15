dc445e0b916f92fd797e18ef4e9c6b2a9a83b9a3
João Pedro <joaopsn1@outlook.com.br>
2024-07-15 17:37:27 -0300

    introdução ao curso

---
+++ Programas/variaveis/src/Variaveis.java
@@ -0,0 +1,7 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        int idade = 22;
+        String nome = "João Pedro";
+        System.out.println("Olá, eu sou o " + nome + " e tenho " + idade + " anos!");
+    }
+}
